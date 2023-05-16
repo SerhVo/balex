e19a6fb3ca4e1e26e6a319da8e9063b0e5474572
SerhVo <94438674+SerhVo@users.noreply.github.com>
2023-05-16 14:26:19 +0000

    Deploying to gh-pages from @ SerhVo/balex@8e0a3e5ed1439c42cb63430fefe38499720a55e3 🚀

--- src/js/paralax.js
+++
@@ -1,6 +0,0 @@
-// window.addEventListener('scroll', function() {
-//   var parallax = document.querySelector('.section_bg_fixed');
-//   var scrollPosition = window.scrollX;
-  
-//   parallax.style.backgroundSize = 10 + scrollPosition/5 + '%';
-// });
--- src/js/rotate.js
+++
@@ -1,4 +0,0 @@
-const img = document.querySelector('.services-tab-img');
-img.addEventListener('click', function () {
-    this.classList.add('active');
-});
