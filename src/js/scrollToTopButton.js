327e8981a3ae2e8e481c05ccb828d3bf6edf9af7
Serhii Vorotnoi <svorotnoy@gmail.com>
2023-05-25 18:13:27 +0300

    fix

--- src/js/scrollToTopButton.js
+++ src/js/scrollToTopButton.js
@@ -2,7 +2,7 @@
 const toTop = document.querySelector(".to-top");
 
 window.addEventListener("scroll", () => {
-  if (window.pageYOffset > 50) {
+  if (window.pageYOffset > 100) {
     toTop.classList.add("activeTo");
   } else {
     toTop.classList.remove("activeTo");
